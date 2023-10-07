654aa1f5bd3948659b30412914de6401a1b31ca6
“ill3ageef” <ill3ageef@gmail.com>
2023-10-07 11:15:27 +0300

    Initial App Stuff

--- website/views.py
+++ website/views.py
@@ -4,5 +4,8 @@ from flask_login import login_required, current_user
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required
 def home():
     return "<h1>Home Page Waassup!</h1>"
+
+
