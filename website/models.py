72d4a20aa5576ff1fa4e81f7e0aeb54fe815c0a5
“ill3ageef” <ill3ageef@gmail.com>
2023-10-08 07:39:56 +0300

    Initial workings of using new frontend

--- website/models.py
+++ website/models.py
@@ -6,7 +6,8 @@ from sqlalchemy.ext.associationproxy import association_proxy
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
+    name = db.Column(db.String(500))
+    decsription = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
